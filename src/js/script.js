d0d7e43d2378423993c4d54810d72b464c622ef5
Ian SOFTEL <iansoftel@gmail.com>
2023-12-04 17:08:57 -0500

    one more....

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,16 @@
-let d = new Date();
-//document.body.innerHTML = "<h1>Ian Today's date is " + d + "</h1>";
+var phonenumber = "UNDEFINED";
+var queryStringKeyPhoneNumber = "phonenumber";
 
-//document.body.innerHTML = "<h1>Drew Today's date is " + location + "</h1>";;
+const urlParams = new URLSearchParams(location.search);
+for (const [key, value] of urlParams) {
+    console.log(`${key}:${value}`);
+}
 
-//var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
-//var obj = {};
-
-// Sample URL
-var urlString = "https://www.example.com/page.html?phonenumber=2141112233";
-
-// Creating URL object for the given URL
-var url = new URL(urlString);
-
-// Retrieving query string values
-var phonenumber = url.searchParams.get("phonenumber");
-console.log(phonenumber); // Prints: Phone number
-
-var x = "This is JavaScript";
+if (urlParams.has(queryStringKeyPhoneNumber)) {
+    phonenumber = urlParams.get(queryStringKeyPhoneNumber);
+}
  
-document.getElementById('demo').innerHTML = x;
+window.onload = function () {
+    var e = document.getElementById("phonenumber");
+    e.innerHTML="Search mnemonic: " + phonenumber;
+    };
